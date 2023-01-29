e077fea43bd8af51a59baad3c3d796752900fa29
Tamal Samanta <tamal_samanta@persistent.com>
2023-01-30 00:48:48 +0530

    backend connectivity

--- Attendance/views.py
+++ Attendance/views.py
@@ -9,7 +9,7 @@ def attendance(request):
     today = datetime.date.today()
     year = today.year
     month = today.month
-    cal = monthcalendar(year, 2)
+    cal = monthcalendar(year, month)
 
     if request.method == "POST":
         res = request.POST
@@ -17,11 +17,11 @@ def attendance(request):
         ec = request.user
         for att in res:
             dt = str(att) + '-' + str(month) + '-' + str(year)
-            print(ec, dt, res[att])
-            # attendance = Attendance()
-            # attendance.EmpCode = ec
-            # attendance.Date = dt
-            # attendance.Status = res[att]
-            # attendance.save(force_update=True)
+            # print(ec, dt, res[att])
+            attendance = Attendance()
+            attendance.EmpCode = ec
+            attendance.Date = dt
+            attendance.Status = res[att]
+            attendance.save(force_update=True)
 
     return render(request, "attendance.html", {'cal':cal})
